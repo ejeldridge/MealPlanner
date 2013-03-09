96df170bb9e5abe62d83b155492b6d1003c1e060
Eric Eldridge <ejeldridge@oakland.edu>
2013-03-08 19:33:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MealPlanner::Application.config.secret_token = 'dd611a8f0d12f16d8528a4f963f29f93dd1d2dfea94771ce2977a23221710cb8673eb2fe05dbd6a2afb2cfaca78d780e54ba0da1525214215be9ad09357f22d9'
