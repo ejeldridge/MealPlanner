7650a5ce43f514ec01a433583f3db3f5f2cd525d
Eric Eldridge <ejeldridge@oakland.edu>
2013-03-12 16:31:58 -0400

    Modified recipes controller and routes to fix syntax

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130312020844) do
+ActiveRecord::Schema.define(:version => 20130311155342) do
 
   create_table "recipes", :force => true do |t|
     t.text     "ingredients"
     t.text     "steps"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.string   "recipename"
   end
 
 end
